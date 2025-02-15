2f37148a825f338cd475738dce0fa07765cb32e2
Micol <micolben@outlook.com>
2025-02-15 16:14:05 +0100

    delete comments with old code

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -26,28 +26,4 @@ const { desc, id, img, ingredients, name, price } = pizza //estraggo valori da d
     )
 }
 
-
-/* dati vecchi
-const Card = (props) => {
-return (
-
-    <div className='pizzaCard'>
-    <div className='pizzaContent'>
-    <img src={props.img} alt='pizza'/>
-    <h2>Pizza {props.name}</h2>
-    <p>Ingredientes:</p>
-    <p>{props.ingredients}</p>
-    <h3>Precio: {props.price} $</h3>
-    </div>
-
-    <div className='buttons'>
-    <button className='button1'>Ver más </button>
-    <button className='button2'>Añadir</button>
-    </div>
-
-    </div>
-)
-}
-*/
-
 export default Card
