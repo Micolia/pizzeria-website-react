8f221c3eecfb7c797b81e4117402bc6d0b4d064f
Micol <micolben@outlook.com>
2025-02-21 11:20:56 +0100

    add new component

---
+++ src/components/Pizza/pizza.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+import './Pizza.css'
+
+const Pizza = () => {
+  const [info, setInfo] = useState(null)
+
+  const getApi = async () => {
+    try {
+      const res = await fetch('http://localhost:5000/api/pizzas/p001')
+      const data = await res.json()
+      setInfo(data)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+    getApi()
+  }, [])
+
+  if (!info) {
+    return <p>Caricamento...</p>;
+  }
+
+  return (
+      <div className="card">
+        <img src={info.img} alt={info.name} />
+        <h2>{info.name}</h2>
+        <p>{info.desc}</p>
+        <div className="pizzaprice">
+          <h3>Prezzo:</h3><h3> ${info.price}</h3>
+        </div>
+        <h3>Ingredienti:</h3>
+        <p>{info.ingredients.join(", ")}</p>
+      </div>
+  )
+}
+
+export default Pizza
