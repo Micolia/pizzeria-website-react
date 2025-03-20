670732e63f022ac01992634ca603915ef34f3302
Micol <micolben@outlook.com>
2025-03-20 13:20:04 +0100

    setup logout

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,6 +1,29 @@
-import './Profile.css'
+import './profile.css'
+import { useContext } from 'react'
+import { UserContext } from '../../context/UserContext'
+import { useNavigate } from 'react-router-dom'
 
 const Profile = () => {
+  const { email, logout } = useContext(UserContext)
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    logout()
+    navigate('/login') // reindirizza a login dopo logout
+  }
+
+  return (
+    <div className='profileblock'>
+      <h2>Perfil de Usuario</h2>
+      <p>Email: {email}</p>
+      <button className='closesession' onClick={handleLogout}>Cerrar sesión</button>
+    </div>
+  )
+}
+
+export default Profile
+
+/* const Profile = () => {
   return (
     <div className='profileblock'>
       <h1>Mi perfil</h1>
@@ -8,6 +31,4 @@ const Profile = () => {
       <button className='closesession'>Cerrar sesión</button>
     </div>
   )
-}
-
-export default Profile
+} */
